96c632e4166fdd256c91e029b091da7046558bcb
hak-code <hammad4898@gmail.com>
2018-11-20 10:42:13 +0500

    sensor meter processing

--- jsProcessing.py
+++ jsProcessing.py
@@ -99,6 +99,7 @@ if os.path.getsize(objectRecieved['fileReceived']) != 0:
                             dictionary[key] += v
 
                 elif os.path.basename(objectRecieved['fileReceived']).startswith('SENSOR'):
+                    print("in sensor")
                     try:
                         dictionary.__delitem__('DAY_ENERGY')
                         dictionary.__delitem__('TOTAL_ENERGY')
@@ -131,7 +132,6 @@ if os.path.getsize(objectRecieved['fileReceived']) != 0:
                 unixTimeStamp = int(time.mktime(datetime.datetime.strptime(timeStamp, "%Y-%m-%d %H:%M:%S").timetuple()))
                 unixTimeStamp = unixTimeStamp * 1000
                 dictionary.__delitem__('Timestamp')
-                print(dictionary)
                 try:
                     for k, v in dictionary.items():
                         if v is not None:
