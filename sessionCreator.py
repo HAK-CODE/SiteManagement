4c7c5c6d55499d7f80d4125f3f7a3733ae178300
hak-code <hammad4898@gmail.com>
2019-01-05 18:53:54 +0500

    udpate

--- sessionCreator.py
+++
@@ -1,7 +0,0 @@
-import libtmux
-
-server = libtmux.Server()
-session = server.new_session("testingnew")
-window = session.attached_window
-window.attached_pane.send_keys("python main.py --siteId='a08394a2-9d25-4126-8adf-a13183f723ee'")
-
