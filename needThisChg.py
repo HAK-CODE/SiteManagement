cefeb9df9a0465b70d94a4c690f255d7b23b3c3f
hak-code <hammad4898@gmail.com>
2019-03-03 22:01:13 +0500

    validation for PAC and inverter length data push.

--- needThisChg.py
+++ needThisChg.py
@@ -79,7 +79,6 @@ if os.path.getsize(objectRecieved['fileReceived']) != 0:
                             if key in data['Body']:
                                 if len(data['Body'][key]['Values'].keys()) == objectRecieved['db']['siteConfig']['siteInfo']['siteInverterQuantity'] or key == 'PAC':
                                     dictionary[key] = 0
-                                    print("now calculating inverter.")
                                     validation = True
                                     for k, v in data['Body'][key]['Values'].items():
                                         dictionary[key] += dictionaryBuilder(key, v)
@@ -120,8 +119,6 @@ if os.path.getsize(objectRecieved['fileReceived']) != 0:
                         unixTimeStamp = int(time.mktime(datetime.datetime.strptime(timeStamp, "%Y-%m-%d %H:%M:%S").timetuple()))
                         unixTimeStamp = unixTimeStamp * 1000
                         dictionary.__delitem__('Timestamp')
-                        print("Dictionary")
-                        print(dictionary)
                         try:
                             for k, v in dictionary.items():
                                 if v is not None:
