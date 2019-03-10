2ee9611e7df2d7e9a4870d2b81fc6dee70242416
hak-code <hammad4898@gmail.com>
2019-03-10 19:37:04 +0500

    update

--- needThisChg.py
+++ needThisChg.py
@@ -85,16 +85,7 @@ if os.path.getsize(objectRecieved['fileReceived']) != 0:
                                         dictionary[key][k] = dictionaryBuilder(key, v)
                                         dictionary[key]['sum'] += dictionaryBuilder(key, v)
                         dictionary['type'] = "inverter"
-                        """
-                        if 'DAY_ENERGY' in dictionary:
-                            dictionary['DAY_ENERGY_OBJ'] = dictionary.pop('DAY_ENERGY')
-                        if 'PAC' in dictionary:
-                            dictionary['PAC_OBJ'] = dictionary.pop('PAC')
-                        if 'TOTAL_ENERGY' in dictionary:
-                            dictionary['TOTAL_ENERGY_OBJ'] = dictionary.pop('TOTAL_ENERGY')
-                        if 'YEAR_ENERGY' in dictionary:
-                            dictionary['YEAR_ENERGY_OBJ'] = dictionary.pop('YEAR_ENERGY')
-                        """
+
                     elif os.path.basename(objectRecieved['fileReceived']).startswith('METER'):
                         for key, value in dictionary.items():
                             if key in data['Body']['0']:
