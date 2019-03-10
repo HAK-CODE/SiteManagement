b98d4905bf2738f7dd3144c8e8ddce00c7c52825
hak-code <hammad4898@gmail.com>
2019-03-10 18:34:16 +0500

    update

--- needThisChg.py
+++ needThisChg.py
@@ -78,10 +78,11 @@ if os.path.getsize(objectRecieved['fileReceived']) != 0:
                         for key, value in dictionary.items():
                             if key in data['Body']:
                                 if len(data['Body'][key]['Values'].keys()) == objectRecieved['db']['siteConfig']['siteInfo']['siteInverterQuantity'] or key == 'PAC':
-                                    dictionary[key] = 0
+                                    dictionary[key]['sum'] = 0
                                     validation = True
                                     for k, v in data['Body'][key]['Values'].items():
-                                        dictionary[key] += dictionaryBuilder(key, v)
+                                        dictionary[key] = dictionaryBuilder(key, v)
+                                        dictionary[key]['sum'] += dictionaryBuilder(key, v)
                         dictionary['type'] = "inverter"
 
                     elif os.path.basename(objectRecieved['fileReceived']).startswith('METER'):
