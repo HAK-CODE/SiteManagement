b46bddb545349b081a7f96c53a5253286c002c21
hak-code <hammad4898@gmail.com>
2019-04-15 15:12:52 +0500

    chg name of logger to siteTag

--- reon-prod-parser.py
+++ reon-prod-parser.py
@@ -183,7 +183,7 @@ if os.path.getsize(objectRecieved['fileReceived']) != 0:
                                 if objectRecieved['db']['siteConfig']['csv'][k]['applyOperation']:
                                     v = (v / objectRecieved['db']['siteConfig']['csv'][k]['multiplier']) + objectRecieved['db']['siteConfig']['csv'][k]['offset']
                             innerDict[k] = v
-                    builder['logger'] = innerDict
+                    builder[objectRecieved['db']['siteConfig']['siteInfo']['siteTag']] = innerDict
                     builder['siteName'] = objectRecieved['db']['siteConfig']['siteInfo']['siteName']
                     save.append(builder)
                 es.loadData(save)
