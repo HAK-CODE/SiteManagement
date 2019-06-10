66c060109b9e4ca1d9df99ba94b8bc1df1b6088a
hak-code <hammad4898@gmail.com>
2019-06-10 22:28:43 +0500

    update

---
+++ alucan_migration.py
@@ -0,0 +1,37 @@
+import sys
+import ftplib
+import os
+import time
+import boto3
+import ntpath
+bucketName = "reon-historian"
+s3 = boto3.client('s3')
+
+while True:
+    server = "ftpreonenergy.qosenergy.com"
+    user = "reonenergy"
+    password = "@Reonenergy92"
+    source = "/ALUCAN/ZL3"
+    destination = "/home/hak/Downloads/files"
+    interval = 0.05
+
+    ftp = ftplib.FTP_TLS(host='ftpreonenergy.qosenergy.com',
+                         passwd="@Reonenergy92",
+                         user="reonenergy")
+
+    try:
+        ftp.cwd("/qos_archives/ALUCAN")
+    except OSError:
+        pass
+    except ftplib.error_perm:
+        print("Error: could not change to ")
+        sys.exit("Ending Application")
+    filelist = ftp.nlst()
+    print(filelist)
+    for file in filelist:
+        if "log" in file and ".csv" in file:
+            ftp.retrbinary("RETR " + file, open(os.path.join(destination, file), "wb").write)
+            response = s3.upload_file(destination+"/"+file, bucketName, "ALUCAN/" + "MIGRATION" + "/" + file)
+            print(response)
+        # ftp.delete(file)
+    ftp.close()
