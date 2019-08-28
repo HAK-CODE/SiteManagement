3dc2b9de2833540ca7d67e347a536f8bfe0ce1fb
Muhammad Faizan <muhammad.faizan@dawoodhercules.com>
2019-08-28 11:51:55 +0500

    s3 file corruption issue resolved

---
+++ fileReleaseXLSX.py
@@ -0,0 +1,31 @@
+import os
+import time
+
+
+class IOoperation:
+    FILENAME = None
+    FILESIZE = None
+    COUNT = 2
+    SLEEP = 5
+
+    def setFile(self, FILENAME):
+        if os.path.isfile(FILENAME):
+            print('file exist')
+            self.FILENAME = FILENAME
+
+    def isFileRelease(self):
+        FLAG = False
+        self.FILESIZE = os.path.getsize(self.FILENAME)
+        while True:
+            self.FILESIZE = os.path.getsize(self.FILENAME)
+            time.sleep(self.SLEEP)
+            if self.COUNT == 0:
+                break
+            currSize = os.path.getsize(self.FILENAME)
+            if currSize != self.FILESIZE:
+                FLAG = False
+            else:
+                FLAG = True
+            if FLAG is not False:
+                self.COUNT -= 1
+        return True
